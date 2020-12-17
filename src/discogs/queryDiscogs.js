9f2020a0b0d78feea00bf92c02c2e63fb4451091
whatjackhasmade <jack@noface.co.uk>
2020-12-17 20:40:04 +0000

    Add rich details on wishlist

--- src/discogs/queryDiscogs.js
+++ src/discogs/queryDiscogs.js
@@ -9,9 +9,18 @@ const queryDiscogs = async () => {
 			.getReleases("jack.davies", 0, { page: 1, per_page: 50 });
 
 		const wants = res.wants;
-		const items = wants.map((item) => {
-			const object = { id: item.id, title: item.basic_information.title };
 
+		const items = wants.map((item) => {
+			const id = item.id;
+			const basic = item.basic_information;
+			const artists = basic.artists;
+			const labels = basic.labels;
+			const title = basic.title;
+
+			const artistsString = Boolean(artists) && artists.join(", ");
+			const labelsString = Boolean(labels) && labels.join(", ");
+
+			const object = { id, artists: artistsString, label: labelsString, title };
 			return object;
 		});
 
