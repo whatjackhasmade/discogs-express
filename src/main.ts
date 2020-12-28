0d0036035b043d6d1758a9f1ac90a219de018b3a
whatjackhasmade <jack@noface.co.uk>
2020-12-28 11:42:39 +0000

    Add get endpoints

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,33 @@ import cron from "node-cron";
 import { Request, Response } from "express";
 
 import { app } from "./app";
+import { getPosts } from "./db";
+import { getWantlist } from "./db";
 import { scanReddit } from "./services";
 import { updateWantlist } from "./services";
 import winstonLogger from "./startup/logger";
 
-const server = http.createServer(app);
+const PORT: number = Number(process.env.PORT) || 5000;
+const env = process.env.NODE_ENV || "dev";
+const startMessage = `${env} server up listening on PORT ${PORT}`;
 
-const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
 
 app.get("/", (req: Request, res: Response) => {
 	res.json({ greet: "hello" });
 });
 
-server.listen(PORT);
+app.get("/posts", async (req: Request, res: Response) => {
+	const data = await getPosts();
+	res.json({ data });
+});
 
-const env = process.env.NODE_ENV || "dev";
-const startMessage = `${env} server up listening on PORT ${PORT}`;
+app.get("/wantlist", async (req: Request, res: Response) => {
+	const data = await getWantlist();
+	res.json({ data });
+});
+
+server.listen(PORT);
 
 server.on("listening", () => {
 	winstonLogger.info(startMessage);
